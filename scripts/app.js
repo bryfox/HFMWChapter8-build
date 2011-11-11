395671a6026ba7a2a4a889b8f53583b66674c233
bryfox <bryan@bryfox.com>
2011-11-10 17:56:10 -0800

    changes with alerts for quick emulator debugging

--- scripts/app.js
+++ scripts/app.js
@@ -4,23 +4,35 @@ $(document).bind("mobileinit", function() {
   });
 });
 
-$(document).ready(function() {
-  document.addEventListener("deviceready", function() {
+if (typeof phonegap != 'undefined') {
+  document.addEventListener("deviceready", onDeviceReady, false);
+} else {
+  $(document).ready(onDeviceReady);
+}
 
-    $('#camera').click(function() {
-      navigator.camera.getPicture(
-        function(imageURI) {
-          alert(imageURI);
-        },
-        function (message) {
-          alert(message);
-        },
-        {
-          quality: 50,
-          destinationType: Camera.DestinationType.FILE_URI
-        }
-      );
-    });
+function onDeviceReady () {
+  var captureSuccess = function(mediaFiles) {
+    var i, src;
+    for (i in mediaFiles) {
+      // Check for the W3C property `url` first.
+      // If not found, try PhoneGap's `fullPath` property (correct as of 1.2)
+      src = data[i].url || data[i].fullPath
+      if (src) $('body').append($('img').attr('src', src));
+    }
+  }
 
-  }, false);
-});
+  var captureError = function(error) {
+    alert(error.message);
+  };
+
+  var captureImage = function() {
+    try {
+      navigator.device.capture.captureImage(captureSuccess, captureError, {limit: 1});
+    } catch (e) {
+      alert(e.message)
+    }
+  };
+
+  alert(navigator.device)
+  $('#camera').on('click', captureImage);
+}
